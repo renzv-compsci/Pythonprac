8a397eb1b0e4e47bfda2ac788edbc0be71affd26
Renz Viloria <viloriarenz917@gmail.com>
2025-05-05 23:55:39 +0800

    Added data grouping, split and explode

--- corePython_dataHandling/simpleDataAnaylzer/movieRatings.py
+++ corePython_dataHandling/simpleDataAnaylzer/movieRatings.py
@@ -15,6 +15,28 @@ print(f"Lowest Rated Movie: {min_rateMovie}")
 max_rateMovie = movieData['Rate'].max()
 print(f"Highest Rated Movie: {max_rateMovie}")
 
-# Highest moview metascore
+# Highest movie metascore
 max_movieMetaScore = movieData['Metascore'].max()
 print(f"Highest Movie Metascore: {max_movieMetaScore}")
+
+# DATA GROUPING 
+
+# Data Spliting and Explode (Average rating per genre)
+movieData['Genre'] = movieData['Genre'].str.split(', ')
+movieData = movieData.explode('Genre') 
+
+# Average rating per genre
+ave_ratingGenre = movieData.groupby('Genre')['Rate'].mean()
+print(f"Average Rating per Genre: {ave_ratingGenre}")
+
+# Highest movie metascore per genre
+max_genreMetaScore = movieData.groupby('Genre')['Metascore'].max()
+print(f"\nHighest metascore per genre: {max_genreMetaScore}")
+
+# Data Splitting and Explode (Average rating per certificate)
+movieData['Certificate'] = movieData['Certificate'].str.split(', ')
+movieData = movieData.explode('Certificate')
+
+# Average rating per certificate
+ave_rateperYear = movieData.groupby('Certificate')['Rate'].mean()
+print(f"\nAverage rating per certifacate: {ave_rateperYear}")
