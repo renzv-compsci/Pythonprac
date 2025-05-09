499ced5fa5a570735da3c3efea7aa3c4625d13d9
Renz Viloria <viloriarenz917@gmail.com>
2025-05-10 02:13:08 +0800

    prac calc

--- corePython_dataHandling/cliCalc/miniCalc.py
+++ corePython_dataHandling/cliCalc/miniCalc.py
@@ -1,1 +1,39 @@
-asd
+import math
+
+print("Welcome to MiniCalc")
+
+
+print("Choose your operation")
+print("1: Addition")
+print("2: Subtraction")
+print("3: Multiplcation")
+print("4: Division")
+
+
+choice = input ("Enter your choice here: ")
+
+
+x = input ("Enter your first number: ")
+y = input ("Enter your second number: ")
+
+x= float(x)
+y= float(y)
+
+if choice == '1': 
+    result = x + y
+
+elif choice == '2':
+    result = x - y
+
+elif choice == '3':
+    result = x * y
+
+elif choice == '4':
+    result = x/y
+
+else:
+    print("Invalid input")
+
+print(f"Answer {result}")
+
+
